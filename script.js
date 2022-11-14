433b9c52f029efff110a24a959c96361a3a75b59
Greg Schuman <gcschuman@hotmail.com>
2022-11-14 12:09:47 -0800

    Moved the qoute placement into the HTLM to be inside of the new quote funciton.

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const quotes = [
 // New quote function
 function newQuote(){
     const randomNumber = Math.floor(Math.random() * (quotes.length));
+   // Placement of the quote into the HTML display element
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber]; 
 }
+
+
+
+
 
 
